e53b700c7b007f40c814afba51dc5c8de8316f91
jagan2255 <jaganvarah22@gmail.com>
2023-02-01 15:33:22 +0530

    update

--- script.js
+++ script.js
@@ -8,3 +8,38 @@ window.onscroll = function() {
       navbar.style.backgroundColor = "transparent";
     }
   };
+
+
+  $(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
+
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            loop:true,
+            margin:10,
+            autoplay:true,
+            autoplayTimeout:2000,
+            autoplayHoverPause:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+          items:4,
+          loop:true,
+          margin:10,
+          autoplay:true,
+          autoplayTimeout:2000,
+          autoplayHoverPause:true
+     
+        }
+    }
+})
